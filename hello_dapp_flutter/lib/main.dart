ea4d2850136c7a07647e4c809ac9959557aea7ac
elaa-guid <rachidalaaguid79@gmail.com>
2025-12-12 00:29:19 +0100

    Update main.dart

--- hello_dapp_flutter/lib/main.dart
+++ hello_dapp_flutter/lib/main.dart
@@ -1,19 +1,27 @@
 // lib/main.dart
-import 'package:flutter/material.dart';
-import 'contract_sim.dart';
-
-void main() => runApp(const MyApp());
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Hello DApp (Simulé)',
-      theme: ThemeData(
-        primarySwatch: Colors.deepPurple,
-      ),
-      home: const HelloSimPage(),
-    );
-  }
+import 'package:flutter/material.dart'; 
+import 'package:geeks_for_geeks/contract_linking.dart'; 
+import 'package:geeks_for_geeks/helloUI.dart'; 
+import 'package:provider/provider.dart'; 
+ 
+void main() { 
+  runApp(MyApp()); 
+} 
+ 
+class MyApp extends StatelessWidget { 
+  @override 
+  Widget build(BuildContext context) { 
+    // Inserting Provider as a parent of HelloUI() 
+    return ChangeNotifierProvider<ContractLinking>( 
+      create: (_) => ContractLinking(), 
+      child: MaterialApp( 
+        title: "Hello World", 
+        theme: ThemeData(
+          brightness: Brightness.dark, 
+          primaryColor: Colors.cyan[400], 
+          hintColor: Colors.deepOrange[200]), 
+        home: HelloUI(), 
+      ), 
+     ); 
+   } 
 }
